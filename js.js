7ff3b733beb9545c2af3095d6aede14fadd1980a
capivara00700 <155832965+capivara00700@users.noreply.github.com>
2024-04-17 19:39:51 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,40 @@
+function botaõAtivo(valor){
+
+
+    let numero = valor
+
+    switch(numero){
+        case '': document.getElementById('resultado').value = ''
+        break
+    
+        case '<': numero-
+        console.log(numero)
+        document.getElementById('resultado').value = numero
+
+        break
+    
+        case '/': document.getElementById('resultado').value += numero
+        break
+    
+        case '*': document.getElementById('resultado').value += numero
+        break
+    
+        case '-': document.getElementById('resultado').value += numero
+        break
+        
+        case '+': document.getElementById('resultado').value += numero
+        break
+    
+        case '=': let resultado = eval(document.getElementById('resultado').value)
+        document.getElementById('resultado').value = resultado
+        break
+    
+        case '.': document.getElementById('resultado').value += numero
+        break
+    
+        default: 
+        document.getElementById('resultado').value += numero
+    }
+
+}
+
